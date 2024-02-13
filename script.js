f73c2fa5f4790b3eaafb960b389b9563b746414b
Max Chechel <maksymchechel@yahoo.com>
2024-02-13 18:32:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,47 @@ const dropdowns = document.querySelectorAll(".filter-dropdown");
 
 dropdowns.forEach((dd) => {
   const ddLabel = dd.querySelector(".filter-dropdown_label");
-  let checkedFilters = dd.querySelectorAll(
-    ".filter-checkbox_button.w--redirected-checked"
-  ).length;
-  // w--redirected-checked
+  const initialLabelText = ddLabel.textContent;
+  console.log(initialLabelText);
+  let checkedFilters = [
+    ...dd.querySelectorAll(".filter-checkbox_button.w--redirected-checked"),
+  ];
+
+  // Setup text of DD on initial load of 1 item selected
+  if (checkedFilters.length === 1) {
+    let parent = checkedFilters[0].closest(".filter-checkbox_field");
+    let labelText = parent.querySelector(".filter-checkbox_label").textContent;
+    ddLabel.textContent = labelText;
+  } else if (checkedFilters.length > 1) {
+    ddLabel.textContent = `${checkedFilters.length} filters`;
+  }
 
   dd.addEventListener("click", (e) => {
     if (e.target.tagName === "LABEL") {
-      const checkBox = e.target.querySelector(".filter-checkbox_button");
-      setTimeout(() => {
-        checkedFilters = dd.querySelectorAll(
-          ".filter-checkbox_button.w--redirected-checked"
-        ).length;
-        console.log(checkedFilters);
-        if (
-          !checkBox.classList.contains("w--redirected-checked") &&
-          checkedFilters === 1
-        ) {
-          ddLabel.textContent = e.target.querySelector(
-            ".filter-checkbox_label"
-          ).textContent;
+      // Use requestAnimationFrame to ensure the code runs after rendering
+      requestAnimationFrame(() => {
+        checkedFilters = [
+          ...dd.querySelectorAll(
+            ".filter-checkbox_button.w--redirected-checked"
+          ),
+        ];
+        let parent = checkedFilters[0]?.closest(".filter-checkbox_field");
+        let labelText = parent?.querySelector(
+          ".filter-checkbox_label"
+        ).textContent;
+
+        if (checkedFilters.length === 1) {
+          ddLabel.textContent = labelText;
         }
-      }, 0);
+
+        if (checkedFilters.length > 1) {
+          ddLabel.textContent = `${checkedFilters.length} filters`;
+        }
+
+        if (checkedFilters.length === 0) {
+          ddLabel.textContent = initialLabelText;
+        }
+      });
     }
   });
 });
