cb026f0256d01d48f02ccaf8810c2838b75d4267
Max Chechel <maksymchechel@yahoo.com>
2024-02-14 10:24:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,82 @@
-const dropdowns = document.querySelectorAll(".filter-dropdown");
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdowns = document.querySelectorAll(".filter-dropdown");
+  const filterMobButtonText = document.querySelector("#filters .button-text");
 
-dropdowns.forEach((dd) => {
-  const reset = dd.closest(".filter_wrapper").querySelector(".filter_reset");
-  const ddLabel = dd.querySelector(".filter-dropdown_label");
-  const initialLabelText = ddLabel.textContent;
-  let checkedFilters = [
-    ...dd.querySelectorAll(".filter-checkbox_button.w--redirected-checked"),
-  ];
-
-  // Setup text of DD on initial load of 1 item selected
-  if (checkedFilters.length === 1) {
-    let parent = checkedFilters[0].closest(".filter-checkbox_field");
-    let labelText = parent.querySelector(".filter-checkbox_label").textContent;
-    ddLabel.textContent = labelText;
-  } else if (checkedFilters.length > 1) {
-    ddLabel.textContent = `${checkedFilters.length} filters`;
-  }
-  reset.addEventListener("click", () => {
-    ddLabel.textContent = initialLabelText;
+  //Set applied filters num on mobile filters button on page load
+  const activeFiltersCounter = document.querySelectorAll(
+    ".filter-checkbox_button.w--redirected-checked"
+  ).length;
+  filterMobButtonText.textContent = activeFiltersCounter
+    ? `Filters(${activeFiltersCounter})`
+    : "Filters";
+  //Open/Close filters modal on mobile
+  document.querySelector("#filters").addEventListener("click", () => {
+    document
+      .querySelector(".custom-t_filters-outer-wrap")
+      .classList.add("is-active");
   });
-  dd.addEventListener("click", (e) => {
-    if (e.target.tagName === "LABEL") {
-      // Use requestAnimationFrame to ensure the code runs after rendering
-      requestAnimationFrame(() => {
-        checkedFilters = [
-          ...dd.querySelectorAll(
-            ".filter-checkbox_button.w--redirected-checked"
-          ),
-        ];
-        let parent = checkedFilters[0]?.closest(".filter-checkbox_field");
-        let labelText = parent?.querySelector(
-          ".filter-checkbox_label"
-        ).textContent;
+  document
+    .querySelector(".custom-t_close-modal")
+    .addEventListener("click", () => {
+      const activeFiltersCounter = document.querySelectorAll(
+        ".filter-checkbox_button.w--redirected-checked"
+      ).length;
+      filterMobButtonText.textContent = activeFiltersCounter
+        ? `Filters(${activeFiltersCounter})`
+        : "Filters";
+      document
+        .querySelector(".custom-t_filters-outer-wrap")
+        .classList.remove("is-active");
+    });
 
-        if (checkedFilters.length === 1) {
-          ddLabel.textContent = labelText;
-        }
+  dropdowns.forEach((dd) => {
+    const reset = dd.closest(".filter_wrapper").querySelector(".filter_reset");
+    const ddLabel = dd.querySelector(".filter-dropdown_label");
+    const initialLabelText = ddLabel.textContent;
+    let checkedFilters = [
+      ...dd.querySelectorAll(".filter-checkbox_button.w--redirected-checked"),
+    ];
 
-        if (checkedFilters.length > 1) {
-          ddLabel.textContent = `${checkedFilters.length} filters`;
-        }
-
-        if (checkedFilters.length === 0) {
-          ddLabel.textContent = initialLabelText;
-        }
-      });
+    // Setup text of DD on initial load of 1 item selected
+    if (checkedFilters.length === 1) {
+      let parent = checkedFilters[0].closest(".filter-checkbox_field");
+      let labelText = parent.querySelector(
+        ".filter-checkbox_label"
+      ).textContent;
+      ddLabel.textContent = labelText;
+    } else if (checkedFilters.length > 1) {
+      ddLabel.textContent = `${checkedFilters.length} filters`;
     }
+    reset.addEventListener("click", () => {
+      ddLabel.textContent = initialLabelText;
+    });
+    dd.addEventListener("click", (e) => {
+      if (e.target.tagName === "LABEL") {
+        // Use requestAnimationFrame to ensure the code runs after rendering
+        requestAnimationFrame(() => {
+          checkedFilters = [
+            ...dd.querySelectorAll(
+              ".filter-checkbox_button.w--redirected-checked"
+            ),
+          ];
+          let parent = checkedFilters[0]?.closest(".filter-checkbox_field");
+          let labelText = parent?.querySelector(
+            ".filter-checkbox_label"
+          ).textContent;
+
+          if (checkedFilters.length === 1) {
+            ddLabel.textContent = labelText;
+          }
+
+          if (checkedFilters.length > 1) {
+            ddLabel.textContent = `${checkedFilters.length} filters`;
+          }
+
+          if (checkedFilters.length === 0) {
+            ddLabel.textContent = initialLabelText;
+          }
+        });
+      }
+    });
   });
 });
